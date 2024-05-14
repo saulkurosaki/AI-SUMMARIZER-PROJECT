4a8aaef85db6d2dc40cc8535ea55e55c82363fff
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-13 20:25:35 -0600

    feat(Demo): implement the search input field container layou

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,7 +3,17 @@ import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
-  return <div>Demo</div>;
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/* Search */}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={() => {}}
+        ></form>
+      </div>
+    </section>
+  );
 };
 
 export default Demo;
